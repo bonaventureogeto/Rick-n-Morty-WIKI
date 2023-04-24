ff17300982255b4064b7f4816e6934636340edee
Bonaventure <bonaogeto1@gmail.com>
2023-04-24 13:19:13 +0300

    cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import React, { useState, useEffect } from "react";
+import Card from "./components/Card/Card";
+import Filter from "./components/Filter/Filter";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  let api = "https://rickandmortyapi.com/api/character/?page=1";
+
+  let [fetchedData, setFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  useEffect(() => {
+    (async () => {
+      let data = await fetch(api).then((res) => res.json());
+      setFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <h1>Rick & Morty WIKI</h1>
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,33 @@
+import styles from "./Card.module.scss";
+
+const Card = ({ results }) => {
+  let display;
+
+  if (results) {
+    display = results.map((result) => {
+      let { id, image, name, status, location } = result;
+
+      return (
+        <div>
+          <div
+            className={`${styles.card} d-flex flex-column justify-content-center`}
+          >
+            <img className={`${styles.img} img-fluid`} src={image} alt="" />
+            <div className={`${styles.content}`}>
+              <div className="fs-5 fw-bold mb-4">{name}</div>
+              <div className="">
+                <div className="fs-6 fw-normal">Last Location</div>
+                <div className="fs-5">{location.name}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  } else {
+    <h1 className="text-center">Loading...</h1>;
+  }
+  return <div>{display}</div>;
+};
+
+export default Card;
