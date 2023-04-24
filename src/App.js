73a21c7a9b76851b0aea3843a5e42cf0e6eb7bad
Bonaventure <bonaogeto1@gmail.com>
2023-04-24 12:16:23 +0300

    app chagnes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
 
 function App() {
   return (
